ffe1c017d1b1bd65310b316f5e367acefa72e757
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-19 15:37:15 -0300

    A program which ask for three inputs and print them out

---
+++ part01/ex_09.py
@@ -0,0 +1,7 @@
+"""
+A program which ask for three inputs and print them out
+"""
+parta = input("The 1st part: ")
+partb = input("The 1st part: ")
+partc = input("The 1st part: ")
+print(parta + "-" + partb + "-" + partc + "!")
