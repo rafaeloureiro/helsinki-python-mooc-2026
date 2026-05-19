8e25509cd13d282d16f32467a1a015cf40fcee96
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-19 15:32:10 -0300

    A program that takes more than one input and then print a message

---
+++ part01/ex_08.py
@@ -0,0 +1,8 @@
+"""
+A program which asks for the user's name and address and then print out the given information.
+"""
+name = input("What is your name?")
+lastname = input("What is your family name?")
+address = input("What is your adress?")
+city = input("What is your city and postal code?")
+print(f"{name} {lastname}\n{address}\n{city}")
