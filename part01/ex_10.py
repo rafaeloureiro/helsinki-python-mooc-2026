8566bc94947bc6a30550d2551e35b0edae07278e
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-19 15:40:22 -0300

    A program which prints out a story after the user give a name and a year

---
+++ part01/ex_10.py
@@ -0,0 +1,6 @@
+"""
+A program which prints out the following story after the user give a name and a year
+"""
+name = input("Please type in a name:")
+year = input("Please type in a year:")
+print(f"{name} is a valiant knight, born in the year {year}. One morning {name} woke up to an awful racket: a dragon was approaching the village. Only {name} could save the village's residents.")
