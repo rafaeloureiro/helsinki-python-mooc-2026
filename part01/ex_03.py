0dc107012a37a6e42b19fbfc74972b6a0bc9d2fc
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-19 15:15:35 -0300

    Print with paragraphs program
    Add a program that prints the lyrics of 'Row, Row, Row Your Boat'.

---
+++ part01/ex_03.py
@@ -0,0 +1,10 @@
+"""
+A program which prints out the following lines in paragraphs
+"""
+
+song = """Row, row, row your boat,
+Gently down the stream.
+Merrily, merrily, merrily, merrily,
+Life is but a dream."""
+
+print(song)
