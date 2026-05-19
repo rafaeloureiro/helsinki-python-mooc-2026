92e14f297cd29295d37715d86e79aa7660e71fec
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-19 15:03:17 -0300

    Print brothers' names in alphabetical order
    Fix variable name typo in the for loop.

---
+++ part01/ex_02.py
@@ -0,0 +1,8 @@
+"""
+Print out the names of the brothers in alphabetical order.
+"""
+
+names = ["Simeoni", "Juhani", "Eero", "Lauri", "Aapo", "Tuomas", "Timo"]
+names.sort()
+for name in nomes:
+    print(nome)
