64a120cd8b4ee57180a029041c508f2e26065b76
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-21 18:14:21 -0300

    A program to greet user with age calculation using f-string

---
+++ part01/ex_15.py
@@ -0,0 +1,6 @@
+"""
+A program which asks the user for their name and year of birth and prints out a more complex message
+"""
+name = input("Please insert your name:")
+year = input("Please insert your birthday year:")
+print(f"Hi {name}, you will be {2021 - int(year)} years old at the end of the year 2021")
