4fdc60fb51697f37f8ea7e127826e438de17f6b3
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-21 18:51:48 -0300

    Bonus calculation program for loyalty card points.
    This program calculates the end of year bonus based on loyalty card points, applying different bonus percentages based on the points total.

---
+++ part01/ex_29.py
@@ -0,0 +1,17 @@
+"""
+A program that calculates the end of year bonus a customer receives on their loyalty card. 
+The bonus is calculated with the following formula:
+
+- If there are less than a hundred points on the card, the bonus is 10 %;
+- In any other case the bonus is 15 %;
+"""
+
+points = float(input("How many points are on your card? "))
+if points < 100:
+    points2 =points * 1.1
+    print("Your bonus is 10 %")
+if points >= 100:
+    points2 = points * 1.15
+    print("Your bonus is 15 %")
+
+print("You now have", points2, "points")
