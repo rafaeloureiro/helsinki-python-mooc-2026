a3ccf38ae7e11bfb4997591ab8733abc6649235f
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-21 18:12:52 -0300

    F-string formula to print and perform calculation using strings
    Add a program to multiply user input by five.

---
+++ part01/ex_14.py
@@ -0,0 +1,5 @@
+"""
+A program which asks the user for a number and then prints out the number multiplied by five.
+"""
+number = int(input("Please insert a number:"))
+print(f"{number} times 5 is {number * 5}")
