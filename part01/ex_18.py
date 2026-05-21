bdd3f8a0b85e4627b7beec55ee42850d1bdbcac6
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-21 18:26:41 -0300

    A program to compute sum and product of two numbers
    Implement a program to calculate sum and product of two numbers.

---
+++ part01/ex_18.py
@@ -0,0 +1,7 @@
+"""
+A program which asks the user for two numbers and then print out the sum and the product of the two numbers.
+"""
+number01 = int(input("Number 1:"))
+number02 = int(input("Number 2:"))
+print(f"The sum of the numbers: {number01 + number02}")
+print(f"The product of the numbers: {number01 * number02}")
