16e2df50eb3183d6f6654bc1d4559550a0d14692
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-21 18:05:38 -0300

    f-string printing a function using integers and calculating it's sum

---
+++ part01/ex_12.py
@@ -0,0 +1,9 @@
+"""
+f-string printing a function using integers and calculating it's sum
+"""
+x = 27
+y = 15
+print(f"{x} + {y} = {x+y}")
+print(f"{x} - {y} = {x-y}")
+print(f"{x} * {y} = {x*y}")
+print(f"{x} / {y} = {x/y}")
