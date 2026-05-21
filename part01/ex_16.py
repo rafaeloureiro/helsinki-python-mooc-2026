889bbb72d7eeaaa7a4cc640ca9e0e29dadb42254
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-21 18:19:36 -0300

    A program to calculate seconds for a given days
    Add a program to convert days to seconds.

---
+++ part01/ex_16.py
@@ -0,0 +1,5 @@
+"""
+A program which asks the user for a number of daysand then prints out the number of seconds in the amount of days given.
+"""
+days = input("How many days?")
+print(f"Seconds in that many days: {int(days)*86400}")
