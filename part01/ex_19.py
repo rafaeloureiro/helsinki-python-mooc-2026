f1d1375c033f817d4cae32125ab5c4e53b916a10
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-21 18:28:16 -0300

    A program to calculate sum and mean of four numbers.
    The program calculates the sum and mean of four user-input numbers.

---
+++ part01/ex_19.py
@@ -0,0 +1,8 @@
+"""
+A program which asks the user for four numbers and then prints out the sum and the mean of the numbers.
+"""
+number01 = int(input("Number 1:"))
+number02 = int(input("Number 2:"))
+number03 = int(input("Number 3:"))
+number04 = int(input("Number 4:"))
+print(f"The sum of the numbers is {number01 + number02 + number03 + number04} and the mean is {(number01 + number02 + number03 + number04)/4}")
