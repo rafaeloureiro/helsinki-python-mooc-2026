01f0b2cd3bc1c0570e67fcd8f3f2bb26054bcf28
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 12:41:27 -0300

    A program that print out whichever of the three letters would be in the middle in alphabetical order.

--- part02/ex_13.py
+++ part02/ex_13.py
@@ -1,3 +1,11 @@
 """
-MODELO
+A program which asks the user for three letters and then print out whichever of the three letters would be in the middle if the letters were in alphabetical order.
 """
+
+letter01 = input("1st letter: ")
+letter02 = input("2nd letter: ")
+letter03 = input("3rd letter: ")
+
+letters = sorted([letter01, letter02, letter03], key=str.lower)
+
+print(f"The letter in the middle is {letters[1]}")
