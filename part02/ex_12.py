d9b439a3598ae21a94a5d40ed0dd9990a79a529e
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 12:38:57 -0300

    A program that return if a given year is a leap year or not.

--- part02/ex_12.py
+++ part02/ex_12.py
@@ -1,3 +1,11 @@
 """
-MODELO
+A program which asks the user for a year, and then prints out whether that year is a leap year or not.
+Any year that is divisible by four is a leap year. 
+However, if the year is additionally divisible by 100, it is a leap year only if it also divisible by 400.
 """
+
+year = int(input("Por favor, digite o ano: "))
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+    print(f"{year} That year is a leap year.")
+else:
+    print(f"{year} That year is not a leap year.")
