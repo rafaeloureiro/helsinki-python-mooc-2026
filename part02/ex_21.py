8c82a68ffcbee4f299f3d5d117f7af27f2ae5931
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 15:53:41 -0300

    While loop that ends if the user digits end or twice the same given word, after it prints its tries.

--- part02/ex_21.py
+++ part02/ex_21.py
@@ -1,3 +1,20 @@
 """
-MODELO
+A program which keeps asking the user for words. If the user types in end, the program should print out the story the words formed, and finish.
+the loop ends also if the user types in the same word twice in a row.
 """
+
+word = input("Please type in a word: ")
+search = ""
+last_word = ""
+
+while True:
+  if word == "end":
+    break
+  elif word == last_word:
+    break
+  else:
+    search += word + " "
+    last_word = word
+    word = str(input("Please type in a word: ")) 
+
+print(f"{search}")
