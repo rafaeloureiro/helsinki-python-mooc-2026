1b3b461b933db39c1a3d55dd07bbed5d1cf6f9ad
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 15:00:46 -0300

    First while loop.

--- part02/ex_15.py
+++ part02/ex_15.py
@@ -1,3 +1,10 @@
 """
-MODELO
+A program that print out the message "hi" and then ask "Shall we continue?" until the user inputs "no". Then the program should print out "okay then" and finish. 
 """
+
+while True:
+    print("hi")
+    ask = input("Shall we continue? ")
+    if ask.lower() == "no":
+        print("okay then")
+        break
