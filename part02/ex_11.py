58c5f3948a9900a4c24514eecb920d4e52c9ea36
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 12:35:34 -0300

    A program that asks for an integer and return a print based on the number given.

--- part02/ex_11.py
+++ part02/ex_11.py
@@ -1,3 +1,14 @@
 """
-MODELO
+A program which asks the user for an integer number. 
+If the number is divisible by three, the program should print out Fizz. 
+If the number is divisible by five, the program should print out Buzz. 
+If the number is divisible by both three and five, the program should print out FizzBuzz.
 """
+
+number = int(input("Number: "))
+if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+elif number % 3 == 0:
+    print("Fizz")
+elif number % 5 == 0:
+    print("Buzz")
