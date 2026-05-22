539b1db2fca1ed51f21d5b60c013376558615b5e
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 11:54:15 -0300

    Using len function to count characters.

--- part02/ex_02.py
+++ part02/ex_02.py
@@ -1,3 +1,9 @@
 """
-MODELO
+Using len function to count characters.
 """
+
+word = input("Please type in a word:")
+count = len(word)
+if count > 1:
+    print(f"There are {count} letters in the word {word}")
+print(f"Thank you!")
