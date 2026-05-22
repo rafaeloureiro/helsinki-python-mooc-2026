d8f794837dcc498b2076709837c1b352ebfc5fbe
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 12:07:58 -0300

    A program which asks the user for a floating point number and then prints out the integer part and the decimal part separately.

--- part02/ex_03.py
+++ part02/ex_03.py
@@ -1,3 +1,9 @@
 """
-MODELO
+A program which asks the user for a floating point number and then prints out the integer part and the decimal part separately.
 """
+
+number = float(input("Please type in a number: "))
+integer_number = int(number)
+decimal_part = number - integer_number
+print(f"Integer part: {integer_number}")
+print(f"Decimal part: {decimal_part}")
