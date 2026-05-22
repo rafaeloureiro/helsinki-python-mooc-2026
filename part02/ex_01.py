9b7c6def6bc9d07fab79890ddff8ff0dabcfb241
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 11:51:12 -0300

    A print with many conditional statements.

--- part02/ex_01.py
+++ part02/ex_01.py
@@ -1,3 +1,12 @@
 """
-MODELO
+A print with many conditional statements.
 """
+
+number = int(input("Please type in a number: "))
+if number>100:
+    print(f"The number was greater than one hundred")
+    number = number - 100
+    print(f"Now its value has decreased by one hundred")
+    print(f"Its value is now {number}")
+print(f"{number} must be my lucky number!")
+print(f"Have a nice day!")
