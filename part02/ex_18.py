e55a23dc2e7077604894679abeb12afd755d567f
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 15:12:55 -0300

    A program to input a password twice and check if both are equal.

--- part02/ex_18.py
+++ part02/ex_18.py
@@ -1,3 +1,14 @@
 """
-MODELO
+A program which asks the user for a password. 
+The program should then ask the user to type in the password again. 
+If the user types in something else than the first password, the program should keep on asking until the user types the first password again correctly.
 """
+
+key = input("Password: ")
+while True:
+    key2 = input("Repeat password: ")
+    if key == key2:
+        print("User account created!")
+        break
+    else:
+        print("They do not match!")
