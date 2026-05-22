715b9acda2da79f52d0acfa73b866562abfc287a
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 12:32:24 -0300

    Verify users's name in order to check any relatives with characters.

--- part02/ex_09.py
+++ part02/ex_09.py
@@ -1,3 +1,13 @@
 """
-MODELO
+A program which asks for the user's name.
+If the name is Huey, Dewey or Louie, the program should recognise the user as one of Donald Duck's nephews.
+In a similar fashion, if the name is Morty or Ferdie, the program should recognise the user as one of Mickey Mouse's nephews.
 """
+
+name = input("Please type in your name: ")
+if name=="Huey" or name=="Dewey" or name=="Louie":
+    print(f"I think you might be one of Donald Duck's nephews.")
+elif name=="Morty" or name=="Ferdie":
+    print(f"I think you might be one of Mickey Mouse's nephews.")
+else:
+     print(f"You're not a nephew of any character I know of.")
