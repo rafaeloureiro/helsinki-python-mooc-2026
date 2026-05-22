ca7bdacbb267c692bb9839f6ff13d8f22b0ef64d
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 12:16:04 -0300

    A program which asks for the names and ages of two persons and print out the name of the elder.

--- part02/ex_06.py
+++ part02/ex_06.py
@@ -1,3 +1,18 @@
 """
-MODELO
+A program which asks for the names and ages of two persons and print out the name of the elder.
 """
+
+print(f"Person 01")
+name01 = input("Name:")
+age01 = int(input("Age:"))
+
+print(f"Person 02")
+name02 = input("Name:")
+age02 = int(input("Age:"))
+
+if age01 == age02:
+    print(f"{name01} and {name02} are the same age")
+elif age01 > age02:
+    print(f"The elder is {name01}")
+elif age02 > age01:
+    print(f"The elder is {name02}")
