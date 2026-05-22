31c59c674aea2fba1150c42b3f2157c4f692c818
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 15:03:04 -0300

    A program that prints the root square of a given integer.

--- part02/ex_16.py
+++ part02/ex_16.py
@@ -1,3 +1,19 @@
 """
-MODELO
+Please write a program which asks the user for integer numbers.
+If the number is below zero, the program should print out the message "Invalid number".
+If the number is above zero, the program should print out the square root of the number using the Python sqrt function.
+In either case, the program should then ask for another number.
+If the user inputs the number zero, the program should stop asking for numbers and exit the loop.
 """
+
+from math import sqrt
+
+while True:   
+    number = int(input("Please type in a number: "))
+    if number == 0:
+        print(f"Exiting...")
+        break
+    elif number < 0:
+        print(f"Invalid number")
+    elif number > 0:
+        print(f"{sqrt(number)}")
