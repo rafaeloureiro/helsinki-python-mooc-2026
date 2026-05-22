94a724c05f2b15c13164991cc34b6f62465948ec
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 12:15:05 -0300

    A program that compare two input values.

--- part02/ex_05.py
+++ part02/ex_05.py
@@ -1,3 +1,13 @@
 """
-MODELO
+A program which asks for two integer numbers and then print out whichever is greater. 
+If the numbers are equal, the program should print a different message.
 """
+
+number01 = int(input("Please type in the first number:"))
+number02 = int(input("Please type in another number:"))
+if number01 == number02:
+    print(f"The numbers are equal!")
+elif number01 > number02:
+    print(f"The greater number was: {number01}")
+elif number02 > number01:
+    print(f"The greater number was: {number02}")
