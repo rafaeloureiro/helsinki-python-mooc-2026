d00f3d8e128ba7b8cc654137cf3b4f75bb551061
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 12:13:15 -0300

    Age maturity check program.

--- part02/ex_04.py
+++ part02/ex_04.py
@@ -1,3 +1,9 @@
 """
-MODELO
+A program which asks the user for their age and then print out a message based on whether the user is of age or not, using 18 as the age of maturity.
 """
+
+age = int(input("How old are you?"))
+if age >= 18:
+    print("You are of age!")
+else:
+    print("You are not of age!")
