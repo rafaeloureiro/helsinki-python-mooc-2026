821950d2d831eeea357bc3b785bdd595e6f0c3e6
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-22 15:09:15 -0300

    A program to print a countdown using while.

--- part02/ex_17.py
+++ part02/ex_17.py
@@ -1,3 +1,14 @@
 """
-MODELO
+A program to print a countdown using while.
 """
+
+number =5
+print("Countdown!")
+while True:
+  print(number)
+  number = number - 1
+  if number == 0:
+    break
+
+print("Now!")
+
