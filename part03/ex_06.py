35567188b30b40d21eeececa1dce9a0797dd34ea
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 19:14:31 -0300

    Implement sum of consecutive numbers up to a limit given
    The program calculates the sum of consecutive numbers until it reaches a user-defined limit.

--- part03/ex_06.py
+++ part03/ex_06.py
@@ -1,5 +1,13 @@
 """
-MODELO
+A program which asks the user to type in a limit. 
+The program then calculates the sum of consecutive numbers (1 + 2 + 3 + ...) until the sum is at least equal to the limit set by the user.
 """
 
-dd
+limit = int(input("Limit: "))
+number = 0
+total_sum = 0 
+
+while total_sum < limit:
+    number += 1
+    total_sum += number
+print(total_sum)
