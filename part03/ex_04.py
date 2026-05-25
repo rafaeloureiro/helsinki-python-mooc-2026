da7de0a6b4a71a0ceadca970948c6ff5b48ff360
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 18:43:52 -0300

    Implement powers of two up to user-defined limit.
    Add functionality to print powers of two up to a user-defined limit.

--- part03/ex_04.py
+++ part03/ex_04.py
@@ -1,5 +1,11 @@
 """
-MODELO
+A program which asks the user to type in an upper limit then prints out numbers so that each subsequent number is the previous one doubled, starting from the number 1. 
+The program prints out powers of two in order.
+The execution of the program finishes when the next number to be printed would be greater than the limit set by the user.
 """
 
-dd
+limit = int(input("Upper limit: "))
+number = 1
+while number <= limit:
+    print(number)
+    number *= 2
