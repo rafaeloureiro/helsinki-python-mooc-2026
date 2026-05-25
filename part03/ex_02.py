7c57b67348953b08199ad03d59cd2e04a2017815
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 18:13:14 -0300

    Countdown program from user input
    Added a countdown program that prompts for a starting number and counts down to zero.

--- part03/ex_02.py
+++ part03/ex_02.py
@@ -1,5 +1,10 @@
 """
-MODELO
+A program that prints a countdown starting with a giving number.
 """
 
-dd
+print("Are you ready?")
+number = int(input("Please type in a number: "))
+while number > 0:
+  print(number)
+  number -= 1
+print("Now!")
