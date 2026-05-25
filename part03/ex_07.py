c037e65b9bbed06043c061f3fe6db29d82335947
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 19:23:02 -0300

    Sum of consecutive numbers until limit given by the user

--- part03/ex_07.py
+++ part03/ex_07.py
@@ -2,6 +2,16 @@
 A program which asks the user to type in a limit. 
 The program then calculates the sum of consecutive numbers (1 + 2 + 3 + ...) until the sum is at least equal to the limit set by the user.
 The program should print the sum of the consecutive numbers and also the calculation performed.
+Assume the number typed in by the user is always equal to 2 or higher
 """
 
-dd
+limit = int(input("Limit: "))
+number = 1
+total_sum = 1
+calculation = "1"
+
+while total_sum < limit:
+    number += 1
+    total_sum += number
+    calculation += f" + {number}"
+print(f"The consecutive sum: {calculation} = {total_sum}")
