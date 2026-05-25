1f120760a2764edcce7cd1cabd9ba0d8f9676323
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 17:57:47 -0300

    Print even number from 2 to 30
    The program prints all even numbers from 2 to 30, each on a new line.

--- part03/ex_01.py
+++ part03/ex_01.py
@@ -1,5 +1,10 @@
 """
-MODELO
+A program which prints out all the even numbers between two and thirty, using a loop. 
+Each number is printed on a separate line.
 """
 
-dd
+number = 2
+while number <= 30:
+  if number % 2 == 0:
+    print(f"{number}")
+  number += 1
