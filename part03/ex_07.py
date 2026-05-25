bff173eab2f4fe48908dc422ef6887a88f8a4ef6
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 19:15:59 -0300

    Add docstring to explain program behavior
    Added a docstring explaining the program's functionality.

--- part03/ex_07.py
+++ part03/ex_07.py
@@ -1,5 +1,7 @@
 """
-MODELO
+A program which asks the user to type in a limit. 
+The program then calculates the sum of consecutive numbers (1 + 2 + 3 + ...) until the sum is at least equal to the limit set by the user.
+The program should print the sum of the consecutive numbers and also the calculation performed.
 """
 
 dd
