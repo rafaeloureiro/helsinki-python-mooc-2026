395675a6293db0bce313bd39c5f90bb61d137c11
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 18:23:02 -0300

    Number printing program with user input as upper limit
    The program prompts the user for an upper limit and prints all integers from 1 to the limit in ascending order, each on a new line.

--- part03/ex_03.py
+++ part03/ex_03.py
@@ -1,5 +1,10 @@
 """
-MODELO
+A program which asks the user for a number then prints out all integer numbers greater than zero but smaller than the input in ascending order.
+Each number is printed in differente lines.
 """
 
-dd
+limit = int(input("Upper limit: "))
+number = 1
+while number < limit:
+    print(number)
+    number += 1
