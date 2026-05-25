3bb1e43f332560e5053f71a9493acb729135e32c
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 18:52:19 -0300

    Enhance number generation with user-defined multiplier
    The program now allows the user to specify a base multiplier for generating numbers, which are printed until exceeding the user-defined limit.

--- part03/ex_05.py
+++ part03/ex_05.py
@@ -1,5 +1,13 @@
 """
-MODELO
+A program which asks the user to type in an upper limit and a number then prints out numbers so that each subsequent number is the previous number one doubled, starting from the number 1. 
+The program prints out powers of n in order.
+The execution of the program finishes when the next number to be printed would be greater than the limit set by the user.
 """
 
-dd
+limit = int(input("Upper limit: "))
+multiplier = int(input("Base: "))
+
+number = 1
+while number <= limit:
+    print(number)
+    number *= multiplier
