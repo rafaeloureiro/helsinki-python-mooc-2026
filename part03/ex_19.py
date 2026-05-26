65c1c9e1393a80fb4393ac7e463bab7e20fb32fe
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 23:04:57 -0300

    Vowels checking in strings
    The program checks for the presence of vowels in user input and prints messages accordingly.

--- part03/ex_19.py
+++ part03/ex_19.py
@@ -1,5 +1,18 @@
 """
-MODELO
+A program which asks the user to input a string then prints out different messages if the string contains any of the vowels a, e or o.
+Assume the input will be in lowercase entirely.
 """
 
-dd
+word = input("Please type in a string: ")
+
+string = input("Please type in a string: ")
+vowels = "aeo"
+index = 0
+ 
+while index < len(vowels):
+    vowel = vowels[index]
+    if vowel in string:
+        print(vowel, "found")
+    else:
+        print(vowel, "not found")
+    index += 1
