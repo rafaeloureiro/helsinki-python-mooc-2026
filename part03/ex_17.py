104bee1553c7ff433fb354e34aebc690ac3411f6
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 22:53:52 -0300

    Substring print inside a loop
    Added functionality to print substrings starting with the first character.

--- part03/ex_17.py
+++ part03/ex_17.py
@@ -1,5 +1,11 @@
 """
-MODELO
+A program which asks the user to type in a string then prints out all the substrings which begin with the first character, from the shortest to the longest.
 """
 
-dd
+word = input("Please type in a string: ")
+length = len(word)
+limit = 1
+
+while limit <= length:
+    print(word[0:limit])
+    limit += 1
