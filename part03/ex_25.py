979f86f087f130a63276c9144bae451bdf103ec1
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:14:43 -0300

    Factorial calculation for a given input.
    The program now calculates the factorial of a user-input integer until a non-positive number is entered.

--- part03/ex_25.py
+++ part03/ex_25.py
@@ -1,3 +1,18 @@
 """
-Modelo
+A program which asks the user to type in an integer number. If the user types in a number equal to or below 0, the execution ends. 
+Otherwise the program prints out the factorial of the number.
 """
+
+while True:
+    number = int(input("Please type in a number: "))
+    if number <=0:
+      print("Thanks and bye!")
+      break
+
+    current_factorial = 1
+    counter = 1
+    while counter <= number:
+        current_factorial *= counter
+        counter += 1
+
+    print(f"The factorial of the number {number} is {current_factorial}")
