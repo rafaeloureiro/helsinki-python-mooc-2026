4b0dff280fc70bed46f96164fe63ead7623e2f62
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 21:43:40 -0300

    Implement string length comparison program

--- part03/ex_09.py
+++ part03/ex_09.py
@@ -1,5 +1,14 @@
 """
-MODELO
+A program which asks the user for two strings and then prints out whichever is the longer of the two - that is, whichever has the more characters. 
+If the strings are of equal length, the program should print out "The strings are equally long".
 """
 
-dd
+input_string01 = input("Please type in string 1:")
+input_string02 = input("Please type in string 2:")
+
+if len(input_string01) > len(input_string02):
+    print(f"{input_string01} is longer")
+if len(input_string02) > len(input_string01):
+    print(f"{input_string02} is longer")
+if len(input_string01) == len(input_string02):
+    print(f"The strings are equally long")
