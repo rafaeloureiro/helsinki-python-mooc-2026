e18beddc5c3f5b18d719ce6fd410acf39d8427fe
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:18:02 -0300

    Alternating integer printing based on user input.
    The program now asks for a number and prints positive integers between 1 and that number, alternating from both ends.

--- part03/ex_27.py
+++ part03/ex_27.py
@@ -1,3 +1,16 @@
 """
-Modelo
+A program which asks the user to type in a number then prints out the positive integers between 1 and the number itself, alternating between the two ends of the range.
 """
+
+limit = int(input("Please type in a number: "))
+
+left = 1
+right = limit
+
+while left <= right:
+    print(left)
+    left += 1
+    
+    if left <= right:
+        print(right)
+        right -= 1
