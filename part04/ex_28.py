df928be8d6dd02fd3d0c08547f8528d28bad8a44
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 16:19:16 -0300

    A function that returns a new list with unique values in ascending order.

--- part04/ex_28.py
+++ part04/ex_28.py
@@ -1,3 +1,7 @@
 """
-MODEL
+A function named distinct_numbers, which takes a list of integers as its argument. 
+The function returns a new list containing the numbers from the original list in order of magnitude, and so that each distinct number is present only once.
 """
+
+def distinct_numbers(numbers : list[int]):
+  return sorted(set(numbers))
