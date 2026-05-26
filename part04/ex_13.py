58497a2daba9e6166a8977b88fe4487eefc8110a
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 13:23:14 -0300

    A program which asks the user for the number of items to be added in a list one by one.

--- part04/ex_13.py
+++ part04/ex_13.py
@@ -1,3 +1,15 @@
 """
-MODEL
+A program which first asks the user for the number of items to be added. 
+Then the program should ask for the given number of values, one by one, and add them to a list in the order they were typed in. 
+Finally, the list is printed out.
 """
+
+my_list = []
+num_items = int(input("How many items: "))
+
+while num_items > 0:
+    item = int(input(f"Item {num_items}: "))
+    my_list.append(item)
+    num_items -= 1
+
+print(my_list)
