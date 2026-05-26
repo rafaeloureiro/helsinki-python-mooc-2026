53f8b8226a28756f1b9c1c1be93c81ac2d63494d
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 11:20:33 -0300

    Functions for first_word, second_word and last_word in a sentence.

--- part04/ex_11.py
+++ part04/ex_11.py
@@ -1,3 +1,18 @@
 """
-MODEL
+Three functions: first_word, second_word and last_word. Each function takes a string argument.
+As their names imply, the functions return either the first, the second or the last word in the sentence they receive as their string argument.
+In each case you may assume the argument string contains at least two separate words, and all words are separated by exactly one space character. 
+There will be no spaces in the beginning or at the end of the argument strings.
 """
+
+def first_word(sentence):
+    words = sentence.split(" ")
+    return words[0]
+
+def second_word(sentence):
+    words = sentence.split(" ")
+    return words[1]
+
+def last_word(sentence):
+    words = sentence.split(" ")
+    return words[-1]
