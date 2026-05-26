e3674aafeb9bbbbb44ec08269943225c81a93744
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 16:23:48 -0300

    The function returns the length of the longest string in a list.

--- part04/ex_29.py
+++ part04/ex_29.py
@@ -1,3 +1,8 @@
 """
-MODEL
+A function named length_of_longest, which takes a list of strings as its argument. 
+The function returns the length of the longest string.
 """
+
+def length_of_longest(mylist: list[str]):
+    longest = max(mylist, key=len)
+    return len(longest)
