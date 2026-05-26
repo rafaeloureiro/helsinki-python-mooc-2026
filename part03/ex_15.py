e5b260d9573421083fb098584b1cd85f71cdf990
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 22:38:35 -0300

    Add string input and formatting for 20 character output
    Implement string formatting to ensure output is 20 characters.

--- part03/ex_15.py
+++ part03/ex_15.py
@@ -1,5 +1,12 @@
 """
-MODELO
+A program which asks the user for a string and then prints it out so that exactly 20 characters are displayed. 
+If the input is shorter than 20 characters, the beginning of the line is filled in with * characters.
+Assume the input string is at most 20 characters long.
 """
 
-dd
+word = str(input("Please type in a string: "))
+
+if len(word) == 20:
+  print(word)
+else:
+  print(f"{(20 - len(word)) * '*'}{word}")
