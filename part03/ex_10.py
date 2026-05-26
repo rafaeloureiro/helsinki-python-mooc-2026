138d8965b017e1df11b31e3630fae2ecd95bf198
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 21:57:07 -0300

    Print a given string in reverse order
    Implement a program to reverse a string and print each character on a new line.

--- part03/ex_10.py
+++ part03/ex_10.py
@@ -1,5 +1,11 @@
 """
-MODELO
+A program which asks the user for a string then prints out the input string in reversed order, from end to beginning. 
+Each character should be on a separate line.
 """
 
-dd
+word = input("Please type in a string: ")
+index = len(word) - 1
+
+while index >= 0:
+    print(word[index])
+    index -= 1
