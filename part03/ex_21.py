fe642db0cb4587a1770ca1d33e10991c86d79a7f
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 23:10:31 -0300

    Substring extraction based on user input
    The program now prints substrings of at least three characters starting with a specified character.

--- part03/ex_21.py
+++ part03/ex_21.py
@@ -1,3 +1,14 @@
 """
-mODEL
+A program that prints out all the substrings which are at least three characters long, and which begin with the character specified by the user. 
+You may assume the input string is at least three characters long.
 """
+
+word = input("Please type in a word: ")
+character = input("Please type in a character: ")
+
+index = 0
+
+while index <= len(word) -3:
+    if word[index] == character:
+        print(word[index:index+3])
+    index += 1
