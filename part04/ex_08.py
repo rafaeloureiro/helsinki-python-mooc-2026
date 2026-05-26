371bf7a83020eedbf19f5dfc26b7ad64ab842e41
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 11:04:53 -0300

    A function called spruce with one argument that prints a spruce

--- part04/ex_08.py
+++ part04/ex_08.py
@@ -1,3 +1,16 @@
 """
-MODEL
+A function named spruce, which takes one argument. The function prints out the text a spruce!, and the a spruce tree, the size of which is specified by the argument.
 """
+
+def spruce(size):
+    print("a spruce!")
+    
+    i = 1
+    while i <= size:
+        spaces = " " * (size - i)
+        stars = "*" * (2 * i - 1)
+        print(f"{spaces}{stars}")
+        i += 1
+        
+    trunk_spaces = " " * (size - 1)
+    print(f"{trunk_spaces}*")
