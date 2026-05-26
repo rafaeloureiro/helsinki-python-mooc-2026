28bddee4aedde0eb1a6fa15df75c42a0debc57f1
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 13:39:28 -0300

    A program to add words to a list.

--- part04/ex_15.py
+++ part04/ex_15.py
@@ -1,3 +1,14 @@
 """
-MODEL
+A program which asks the user for words. 
+If the user types in a word for the second time, the program should print out the number of different words typed in, and exit.
 """
+
+words = []
+
+while True:
+    word_input = input("Word: ")
+
+    if word_input in words:
+        print(f"You typed in {len(words)} different words")
+        break
+    words.append(word_input)
