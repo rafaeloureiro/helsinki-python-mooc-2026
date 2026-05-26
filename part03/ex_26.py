a88085b367a0a0a4c1b083fca16989cb853f5e71
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:16:30 -0300

    Modify number printing to flip pairs.
    Update the program to print positive integers in flipped pairs.

--- part03/ex_26.py
+++ part03/ex_26.py
@@ -1,3 +1,16 @@
 """
-Modelo
+A program which asks the user to type in a number then prints out all the positive integer values from 1 up to the number. 
+However, the order of the numbers is changed so that each pair or numbers is flipped. That is, 2 comes before 1, 4 before 3 and so forth.
 """
+
+limit = int(input("Please type in a number: "))
+i = 1
+
+while i <= limit:
+    if i + 1 <= limit:
+        print(i + 1)
+        print(i)
+    else:
+        print(i)
+    
+    i += 2
