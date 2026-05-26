bc3a96889eed263bd70d427c9273bd368590f367
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:10:54 -0300

    Print first letters of words
    The program prompts the user for a sentence and prints the first letter of each word on a new line.

--- part03/ex_24.py
+++ part03/ex_24.py
@@ -1,3 +1,13 @@
 """
-Modelo
+A program which asks the user to type in a sentence then prints out the first letter of each word in the sentence, each letter on a separate line.
 """
+
+sentence = input("Please type in a sentence: ")
+ 
+sentence = " " + sentence
+ 
+index = 1
+while index < len(sentence):
+    if sentence[index-1] == " " and sentence[index] != " ":
+        print(sentence[index])
+    index += 1
