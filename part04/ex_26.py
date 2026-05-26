00e4c41d2f872eef7eac1a1e6e864c6a479b1153
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 15:37:22 -0300

    A program that sums only even numbers in a list.

--- part04/ex_26.py
+++ part04/ex_26.py
@@ -1,3 +1,11 @@
 """
-MODEL
+A function named even_numbers, which takes a list of integers as an argument. 
+The function returns a new list containing the even numbers from the original list.
 """
+
+def even_numbers(numbers : list[int]):
+    sum = 0
+    for num in numbers:
+        if num % 2 == 0:
+            sum += num
+    return sum
