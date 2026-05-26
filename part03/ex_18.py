03122694dbe2e1c2d8f63f8ae77a52c71797e043
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 23:02:10 -0300

    Print substrings in reverse
    The program prints all substrings ending with the last character, from shortest to longest.

--- part03/ex_18.py
+++ part03/ex_18.py
@@ -1,5 +1,10 @@
 """
-MODELO
+A program which asks the user to type in a string then prints out all the substrings which end with the last character, from the shortest to the longest. 
 """
 
-dd
+string = input("Please type in a string: ")
+ 
+start = len(string) - 1
+while start >= 0:
+    print(string[start:])
+    start -= 1
