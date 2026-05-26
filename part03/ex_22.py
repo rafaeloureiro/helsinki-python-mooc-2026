433f4d383322cf93f4330e0356fdc1cb2cce165e
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 23:11:46 -0300

    Add substring occurrence finder program
    Implement a program to find the second occurrence of a substring in a string, handling cases where the substring does not occur twice.

--- part03/ex_22.py
+++ part03/ex_22.py
@@ -1,3 +1,20 @@
 """
-mODEL
+A program which finds the second occurrence of a substring. 
+If there is no second (or first) occurrence, the program should print out a message accordingly.
+In this exercise the occurrences cannot overlap. For example, in the string aaaa the second occurrence of the substring aa is at index 2.
 """
+
+text = input("Please type in a string: ")
+sub = input("Please type in a substring: ")
+
+index1 = text.find(sub)
+
+if index1 != -1:
+    index2 = text.find(sub, index1 + len(sub))
+    
+    if index2 != -1:
+        print(f"The second occurrence of the substring is at index {index2}.")
+    else:
+        print("The substring does not occur twice in the string.")
+else:
+    print("The substring does not occur twice in the string.")
