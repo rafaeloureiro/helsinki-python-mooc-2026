e04c83d53b13567ef8ad1926f74d9d90b9f5f9bc
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 10:27:59 -0300

    A program which asks the user which editor they are using.

--- part04/ex_01.py
+++ part04/ex_01.py
@@ -1,3 +1,15 @@
 """
-MODEL
+A program which asks the user which editor they are using. 
+The program should keep on asking until the user types in Visual Studio Code.
 """
+
+while True:
+    editor = input("Editor: ").lower()
+
+    if editor == "visual studio code":
+        print("an excellent choice!")
+        break
+    elif editor == "word" or editor == "notepad":
+        print("awful")
+    else:
+        print("not good")
