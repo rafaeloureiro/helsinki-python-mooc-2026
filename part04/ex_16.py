b076dc6a9220254b27e2149eb0a1c097ce0a567c
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 14:38:06 -0300

    Add a given integer to a list then print the list in order added and um ascending order.

--- part04/ex_16.py
+++ part04/ex_16.py
@@ -1,3 +1,19 @@
 """
-MODEL
+A program which asks the user to type in values and adds them to a list. After each addition, the list is printed out in two different ways:
+- in the order the items were added
+- ordered from smallest to greatest
+The program exits when the user types in 0.
 """
+
+numbers = []
+
+while True:
+    new = int(input("New item: "))
+
+    if new == 0:
+        print("Bye!")
+        break   
+
+    numbers.append(new)
+    print(f"The list now: {numbers}")
+    print(f"The list in order: {sorted(numbers)}")
