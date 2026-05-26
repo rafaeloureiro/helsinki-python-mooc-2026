a7aca35aa13bc64124d5554922d7710a3cf67763
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 15:22:00 -0300

    A loop to print non-zero range from -N to N to a given N by the user.

--- part04/ex_21.py
+++ part04/ex_21.py
@@ -1,3 +1,9 @@
 """
-MODEL
+A program which asks the user for a positive integer N. 
+The program then prints out all numbers between -N and N inclusive, but leaves out the number 0. Each number should be printed on a separate line.
 """
+
+number = int(input("Please type in a positive integer: "))
+for i in range(-number, number+1):
+    if i != 0:
+        print(i)
