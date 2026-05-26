4758ab1c09992a2bf80af02fea1842b9a8747a35
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 15:25:42 -0300

    Anagram check for input words by the user.

--- part04/ex_23.py
+++ part04/ex_23.py
@@ -1,3 +1,11 @@
 """
-MODEL
+A function named anagrams, which takes two strings as arguments. 
+The function returns True if the strings are anagrams of each other. 
+Two words are anagrams if they contain exactly the same characters.
 """
+
+def anagrams(word1,word2):
+    if sorted(word1) == sorted(word2):
+        return True
+    else:        
+        return False   
