2812277ef64cec3cef83648d50d3c7232c5850e7
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 14:48:46 -0300

    A function which takes a list of integers and returns the arithmetic mean.

--- part04/ex_18.py
+++ part04/ex_18.py
@@ -1,3 +1,7 @@
 """
-MODEL
+A function named mean, which takes a list of integers as an argument and returns the arithmetic mean of the values in the list.
 """
+
+def mean(lst : list):
+    list = sum(lst)/len(lst)
+    return list
