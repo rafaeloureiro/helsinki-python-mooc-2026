1b48b833062981af68761badf290da3f6d0561f5
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:30:10 -0300

    Mean function for three integers.
    Add a function to calculate and print the mean of three integers.

--- part03/ex_30.py
+++ part03/ex_30.py
@@ -1,3 +1,7 @@
 """
-Modelo
+A function named mean, which takes three integer arguments. 
+The function should print out the arithmetic mean of the three arguments.
 """
+
+def mean(a,b,c):
+    print(f"{(a+b+c)/3}")
