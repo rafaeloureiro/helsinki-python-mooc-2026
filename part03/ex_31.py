9b7bc93312e065bcc257b567c1709825eebf5434
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:37:11 -0300

    print_many_times function
    Add a function to print a string multiple times

--- part03/ex_31.py
+++ part03/ex_31.py
@@ -1,3 +1,9 @@
 """
-Modelo
+a function named print_many_times(text, times), which takes a string and an integer as arguments. 
+The integer argument specifies how many times the string argument should be printed out:
 """
+
+def print_many_times(text, times):
+    while times > 0:
+        print(text)
+        times -=1
