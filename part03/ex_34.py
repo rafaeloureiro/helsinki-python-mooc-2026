2ba918295160b8ced5d5a2bef2d4d80bddd64093
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:40:45 -0300

    squared function for character square output.
    Add a function to print a square of characters based on input size.

--- part03/ex_34.py
+++ part03/ex_34.py
@@ -1,3 +1,14 @@
 """
-Modelo
+A function named squared, which takes a string argument and an integer argument, and prints out a square of characters
 """
+
+def squared(characters, size):
+    i = 0
+    row = ""
+    while i < size * size:
+        if i > 0 and i % size == 0:
+            print(row)
+            row = ""
+        row += characters[i % len(characters)]
+        i += 1
+    print(row)
