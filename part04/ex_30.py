7d929234ad38bf5ef9e21ff65db0d0d1b1c3ebc5
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 16:40:08 -0300

    The function returns the length of the shortest string in a list.

--- part04/ex_30.py
+++ part04/ex_30.py
@@ -1,3 +1,9 @@
 """
-MODEL
+A function named shortest, which takes a list of strings as its argument then returns whichever of the strings is the shortest. 
+If more than one are equally short, the function can return any of the shortest strings (there will be no such situation in the tests). 
+You may assume there will be no empty strings in the list.
 """
+
+def shortest (mylist: list[str]):
+    shortest = min(mylist, key=len)
+    return shortest
