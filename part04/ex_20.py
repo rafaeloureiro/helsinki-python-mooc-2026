c8774a053cec021f34e6dca0801a2d36cfed6038
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 15:17:53 -0300

    A program that prints each input character on a separate line.

--- part04/ex_20.py
+++ part04/ex_20.py
@@ -1,3 +1,8 @@
 """
-MODEL
+A program which asks the user to type in a string. 
+The program then prints each input character on a separate line. After each character there should be a star (*) printed on its own line.
 """
+
+string = input("Please type in a string: ")
+for character in string:
+    print(f"{character}\n*")
