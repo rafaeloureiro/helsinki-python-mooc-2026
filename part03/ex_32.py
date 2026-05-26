a3fef145d329f9e703774e21b3c5c0c0ccc7d871
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:38:10 -0300

    hash_square function to print hash square.
    Added a function to print a square of hash characters based on the given length.

--- part03/ex_32.py
+++ part03/ex_32.py
@@ -1,3 +1,11 @@
 """
-Modelo
+A function named hash_square(length), which takes an integer argument. 
+The function prints out a square of hash characters, and the argument specifies the length of the side of the square.
 """
+
+def hash_square(length):
+    index = length
+
+    while length > 0:
+        print(f"{'#'*index}")
+        length -= 1
