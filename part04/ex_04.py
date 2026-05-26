cae471456a09372b9f8f03b5550286d73b36b1b7
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 10:43:29 -0300

    A function named square_of_hashes, which draws a square of hash characters.

--- part04/ex_04.py
+++ part04/ex_04.py
@@ -1,3 +1,20 @@
 """
-MODEL
+A function named square_of_hashes, which draws a square of hash characters. 
+The function takes one argument, which determines the length of the side of the square.
+The function should call the function line from the exercise prior for the actual printing out. 
+Copy your solution to that exercise prior the code for this exercise. Please don't change anything in the line function.
 """
+
+def line(length, text):
+    if text == "":
+        character = "*"
+    else:
+        character = text[0]
+    
+    print(f"{character * length}")
+
+def square_of_hashes(size):
+    length = size
+    while length > 0:
+        line(size, "#")
+        length -= 1
