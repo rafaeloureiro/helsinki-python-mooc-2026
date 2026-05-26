bcc32cd68e8743ee714bbafb246fda2aafc7793e
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 23:07:45 -0300

    Character slicing based on user input
    Added functionality to slice and print three characters from a specified starting character in the input string, with error handling for insufficient length.

--- part03/ex_20.py
+++ part03/ex_20.py
@@ -1,5 +1,14 @@
 """
-MODELO
+A program which asks the user to type in a string and a single character then prints the first three character slice which begins with the character specified by the user. 
+You may assume the input string is at least three characters long. The program must print out three characters, or else nothing.
+Pay special attention to when there are less than two characters left in the string after the first occurrence of the character looked for.
+In that case nothing should be printed out, and there should not be any indexing errors when executing the program.
 """
 
-dd
+word = input("Please type in a word: ")
+letter = input("Please type in a character: ")
+
+index = word.find(letter)
+
+if index >= 0 and len(word) >= index + 3:
+    print(word[index:index+3])
