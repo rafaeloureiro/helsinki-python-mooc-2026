c3e89382944d7dc858d545969a65587b50f84d79
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 11:06:19 -0300

    A function returns the greatest in value of the three arguments.

--- part04/ex_09.py
+++ part04/ex_09.py
@@ -1,3 +1,11 @@
 """
-MODEL
+A function named greatest_number, which takes three arguments. The function returns the greatest in value of the three.
 """
+
+def greatest_number(a, b, c):
+    if a >= b and a >= c:
+        return a
+    elif b >= a and b >= c:
+        return b
+    else:
+        return c
