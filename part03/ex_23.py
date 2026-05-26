639718536ec2274b7f22fe7e92d28f463fa1fac4
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:05:14 -0300

    Multiplication loop logic.
    Fix the program to correctly iterate through both counters for multiplication.

--- part03/ex_23.py
+++ part03/ex_23.py
@@ -1,3 +1,12 @@
 """
-Modelo
+A program which asks the user for a positive integer number then prints out a list of multiplication operations until both operands reach the number given by the user.
 """
+
+number = int(input("Please type in a number: "))
+counter1 = 1
+while counter1 <= number:
+    counter2 = 1
+    while counter2 <= number:
+        print(f"{counter1} x {counter2} = {counter1*counter2}")
+        counter2 += 1
+    counter1 += 1
