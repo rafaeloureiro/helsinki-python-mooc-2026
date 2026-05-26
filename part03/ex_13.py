dacee549b1ac156a9bfd8c275d2155c99a77aaa6
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 22:18:26 -0300

    Rectangle of hashes print based on user input

--- part03/ex_13.py
+++ part03/ex_13.py
@@ -1,5 +1,10 @@
 """
-MODELO
+A program which prints out a ractangle of hash characters, the width and height are chosen by the user.
 """
 
-dd
+width = int(input("Width: "))
+height = int(input("Width: "))
+
+while height > 0:
+    print(f"{width*'#'}")
+    height -= 1
