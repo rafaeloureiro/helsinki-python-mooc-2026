b3f11c17035d2a9d35465f0c207b929a5f765aa5
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:39:40 -0300

    chessboard function to print pattern
    Added a function to print a chessboard pattern using ones and zeroes based on the specified size.

--- part03/ex_33.py
+++ part03/ex_33.py
@@ -1,3 +1,14 @@
 """
-Modelo
+A function named chessboard, which prints out a chessboard made out of ones and zeroes. 
+The function takes an integer argument, which specifies the length of the side of the board.
 """
+
+def chessboard(size):
+    i = 0
+    while i < size:
+        if i % 2 == 0:
+            row = "10"*size
+        else:
+            row = "01"*size
+        print(row[0:size])
+        i += 1
