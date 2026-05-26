6ee1d537aa589ccfa3e2416930095044cea0e2d7
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 10:58:37 -0300

    A function named triangle, which draws a triangle of hashes, and takes one argument.

--- part04/ex_06.py
+++ part04/ex_06.py
@@ -1,3 +1,20 @@
 """
-MODEL
+A function named triangle, which draws a triangle of hashes, and takes one argument. 
+The triangle should be as tall and as wide as the value of the argument.
+The function should call the function line from the exercise prior for the actual printing out. 
+Copy your solution to that exercise prior the code for this exercise. Please don't change anything in the line function.
 """
+
+def line(length, text):
+    if text == "":
+        character = "*"
+    else:
+        character = text[0]
+    
+    print(f"{character * length}")
+
+def triangle(size):
+    i = 1
+    while i <= size:
+        line(i, "#")
+        i += 1
