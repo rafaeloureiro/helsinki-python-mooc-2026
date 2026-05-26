6c2fa805daf59c8b2118a00b500aa69ad546dc5b
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 11:09:07 -0300

    Implement same_chars function to compare characters at specific indexes.

--- part04/ex_10.py
+++ part04/ex_10.py
@@ -1,3 +1,10 @@
 """
-MODEL
+A function named same_chars, which takes one string and two integers as arguments. 
+The integers refer to indexes within the string. The function should return True if the two characters at the indexes specified are the same. 
+Otherwise, and especially if either of the indexes falls outside the scope of the string, the function returns False.
 """
+
+def same_chars(word, index1, index2):
+    if 0 <= index1 < len(word) and 0 <= index2 < len(word):
+        return word[index1] == word[index2]
+    return False
