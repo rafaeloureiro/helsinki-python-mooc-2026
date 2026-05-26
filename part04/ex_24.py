6dac2d401f46de795b5e6cd3a81a1d3618c6ccd5
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 15:28:03 -0300

    Palindrome check for given words.

--- part04/ex_24.py
+++ part04/ex_24.py
@@ -1,3 +1,17 @@
 """
-MODEL
+A function named palindromes, which takes a string argument and returns True if the string is a palindrome. 
+Palindromes are words which are spelled exactly the same backwards and forwards.
 """
+
+def palindromes(word: str):
+    return word == word[::-1]
+
+
+while True:
+    guess = input("Please type in a palindrome: ")
+    
+    if palindromes(guess):
+        print(f"{guess} is a palindrome!")
+        break 
+    else:
+        print("that wasn't a palindrome")
