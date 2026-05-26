11b4074602396c32b83754f4df93f2c48990c1a7
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 14:50:08 -0300

    A function returns the difference between the smallest and the largest value in the list.

--- part04/ex_19.py
+++ part04/ex_19.py
@@ -1,3 +1,8 @@
 """
-MODEL
+A function named range_of_list, which takes a list of integers as an argument. 
+The function returns the difference between the smallest and the largest value in the list.
 """
+
+def range_of_list (lst):
+    list = max(lst) - min(lst) 
+    return list
