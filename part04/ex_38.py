625bd043f41fc55143d9c1dcbc6285b542c89777
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 17:01:47 -0300

    Create ex_38.py

---
+++ part04/ex_38.py
@@ -0,0 +1,9 @@
+"""
+A program for printing out grade statistics for a university course.
+The program asks the user for results from different students on the course. These include exam points and numbers of exercises completed. 
+The program then prints out statistics based on the results.
+Exam points are integers between 0 and 20. The number of exercises completed is an integer between 0 and 100.
+The program keeps asking for input until the user types in an empty line. 
+You may assume all lines contain valid input, which means that there are two integers on each line, or the line is empty.
+"""
+
