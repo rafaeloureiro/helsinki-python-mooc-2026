6d3a0ffdc41b2e3080aad05bd624d745afe6459e
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 05:27:27 -0300

    seven_brothers function to print names
    Added a function to print the names of seven brothers.

--- part03/ex_28.py
+++ part03/ex_28.py
@@ -1,3 +1,12 @@
 """
-Modelo
+A function named seven_brothers. When the function is called, it should print out the names of the seven brothers in alphabetical order.
 """
+
+def seven_brothers():
+    print("Aapo")
+    print("Eero")
+    print("Juhani")
+    print("Lauri")
+    print("Simeoni")
+    print("Timo")
+    print("Tuomas")
