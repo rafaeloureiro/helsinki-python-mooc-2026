0af821a869adf2530c18a992707e9604e2f74593
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 22:04:18 -0300

    Character comparison in a user input.
    Add functionality to compare second and second to last characters of user input.

--- part03/ex_11.py
+++ part03/ex_11.py
@@ -1,5 +1,10 @@
 """
-MODELO
+A program which asks the user for a string then prints out a message based on whether the second character and the second to last character are the same or not.
 """
 
-dd
+input01 = str(input("Please type in a string: "))
+index = len(input01)-2
+if input01[1] == input01[index]:
+  print(f"The second and the second to last characters are {input01[index]}")
+else:
+  print(f"The second and the second to last characters are different")
