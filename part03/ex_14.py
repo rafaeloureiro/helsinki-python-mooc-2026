b715b52627d53c92d8f46000e67cefb8d267dde4
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-25 22:22:00 -0300

    Print underlined words
    This program prompts the user for strings in a loop and prints each string underlined. It terminates when an empty string is entered.

--- part03/ex_14.py
+++ part03/ex_14.py
@@ -1,5 +1,14 @@
 """
-MODELO
+A program which asks the user for strings using a loop.
+The program prints out each string underlined
+The execution ends when the user inputs an empty string - that is, just presses Enter at the prompt.
 """
 
-dd
+while True:
+    text = input("Please type in a string: ")
+
+    if text == "":
+        break
+
+    print(text)
+    print("-" * len(text))
