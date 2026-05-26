3f9588bfd5d7fd5595b866ac75fd342372871513
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-26 15:35:04 -0300

    A program that sums only positive numbers in a list.

--- part04/ex_25.py
+++ part04/ex_25.py
@@ -1,3 +1,11 @@
 """
-MODEL
+A function named sum_of_positives, which takes a list of integers as its argument. 
+The function returns the sum of the positive values in the list.
 """
+
+def sum_of_positives(numbers : list[int]):
+    soma = 0
+    for num in numbers:
+        if num > 0:
+            soma += num
+    return soma
