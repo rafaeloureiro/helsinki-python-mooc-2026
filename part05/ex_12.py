b6fb78ea0d82332d73130304b1414001b5769f43
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-29 20:25:18 -0300

    Tic-Tac-Toe game.
    Implement play_turn function for tic tac toe game.

--- part05/ex_12.py
+++ part05/ex_12.py
@@ -1,3 +1,13 @@
 """
-MODELO
+tic tac toe game
 """
+
+def play_turn(game_board: list, x: int, y: int, piece: str):
+    if x < 0 or x > 2 or y < 0 or y > 2:
+        return False
+    
+    if game_board[y][x] == "":
+        game_board[y][x] = piece
+        return True
+    
+    return False
