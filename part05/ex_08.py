04d451e5519d861ba25fb5079fd92c48b0abbd9b
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-29 19:07:46 -0300

    A function that contains all values from the original list doubled.

--- part05/ex_08.py
+++ part05/ex_08.py
@@ -1,3 +1,11 @@
 """
-MODELO
+A function named double_items(numbers: list), which takes a list of integers as its argument.
+
+The function should return a new list, which contains all values from the original list doubled. The function should not change the original list.
 """
+
+def double_items(numbers: list):
+    numbers_doubled = []
+    for number in numbers:
+        numbers_doubled.append(number * 2)
+    return numbers_doubled
