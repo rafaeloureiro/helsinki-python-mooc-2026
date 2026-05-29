4d5d42f8f431fdc7faeefa58b728cf9577a1f6ea
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-29 18:05:04 -0300

    Checks if a specific column in a Sudoku grid is filled in correctly.

--- part05/ex_05.py
+++ part05/ex_05.py
@@ -5,17 +5,11 @@ A column is valid if it contains each of the numbers 1 to 9 at most once.
 Empty squares (represented by 0) are ignored and can appear multiple times.
 """
 def column_correct(sudoku: list, column_no: int):
-    column = sudoku[row_no]
-    
-    seen_numbers = []
-    
-    for item in column:
-        if item == 0:
-            continue
-            
-        if item in seen_numbers:
+    numbers = []
+    for row in sudoku:
+        number = row[column_no]
+        if number > 0 and number in numbers:
             return False
-            
-        seen_numbers.append(item)
-        
+        numbers.append(number)
+ 
     return True
