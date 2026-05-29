210f878fb041d9a7269287debdbe049122df48f0
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-29 20:31:08 -0300

    A function that transpose a given matrix.

--- part05/ex_13.py
+++ part05/ex_13.py
@@ -1,3 +1,14 @@
 """
-MODELO
+A function that transpose a given matrix.
+Assume that the matrix is a square matrix.
 """
+
+def transpose(matrix: list):
+    transposed = []
+    
+    for i in range(len(matrix[0])):
+        row = []
+        for j in range(len(matrix)):
+            row.append(matrix[j][i])
+        transposed.append(row)
+    matrix[:] = transposed
