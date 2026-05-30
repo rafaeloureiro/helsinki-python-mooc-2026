d20c8bdd92add57f28f56891a5beba9aac651124
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-30 20:40:08 -0300

    A function which adds a new movie object into a movie database.

--- part05/ex_21.py
+++ part05/ex_21.py
@@ -1,3 +1,6 @@
 """
-MODELO
+A function named add_movie(database: list, name: str, director: str, year: int, runtime: int), which adds a new movie object into a movie database.
 """
+
+def add_movie(database: list, name: str, director: str, year: int, runtime: int):
+    database.append({"name": name, "director": director, "year": year, "runtime": runtime})
