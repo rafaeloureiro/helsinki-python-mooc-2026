bcf0307c7619cbfbd47b80a4f7f96034b6ff8041
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-30 12:54:12 -0300

    A function that inverts (key, value) value in a dictionary.

--- part05/ex_19.py
+++ part05/ex_19.py
@@ -1,3 +1,12 @@
 """
-MODELO
+A function that inverts (key, value) value in a dictionary.
 """
+
+def invert(dictionary: dict):
+	copy = {}
+	for key in dictionary:
+		copy[key] = dictionary[key]
+	for key in copy:
+		del dictionary[key]
+	for key in copy:
+		dictionary[copy[key]] = key
