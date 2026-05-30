fa6b5cb828bb3f3da60bc15a9d029576c9cad9aa
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-30 00:12:38 -0300

    Calculating a new dictionary based on a given dictionary range.

--- part05/ex_14.py
+++ part05/ex_14.py
@@ -1,3 +1,9 @@
 """
-MODELO
+A function that creates a new dictionary that it's values are the original dictionary times 10
 """
+
+def times_ten(start_index: int, end_index: int):
+    numbers = {}
+    for i in range(start_index, end_index + 1):
+        numbers[i] = i * 10
+    return numbers
