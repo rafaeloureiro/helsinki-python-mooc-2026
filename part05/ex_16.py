c1dfb23dcebb9aa6aeda1a473a62c41b3f051a15
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-30 11:56:40 -0300

    Implement histogram function for letter frequency.
    Added a function to print a histogram of letter occurrences in a string.

--- part05/ex_16.py
+++ part05/ex_16.py
@@ -1,3 +1,12 @@
 """
-MODELO
+A function named histogram, which takes a string as its argument. 
+The function should print out a histogram representing the number of times each letter occurs in the string. 
+Each occurrence of a letter should be represented by a star on the specific line for that letter.
 """
+
+def histogram (text: str):
+    char_list = []
+    for char in text:
+        if char not in char_list:
+            char_list.append(char)
+            print(f"{char} " + '*' * text.count(char))
