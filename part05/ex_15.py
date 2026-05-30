efa2d762fa044a091d778a71de1e920c5651d5e4
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-30 00:22:02 -0300

    A function named factorials which returns the factorials of the numbers 1 to n in a dictionary.

--- part05/ex_15.py
+++ part05/ex_15.py
@@ -1,3 +1,12 @@
 """
-MODELO
+A function named factorials(n: int), which returns the factorials of the numbers 1 to n in a dictionary.
 """
+
+def factorials(end_index: int, start_index: int = 1) -> dict:
+    result = {}
+    for i in range(start_index, end_index + 1):
+        factorial = 1
+        for j in range(1, i + 1):
+            factorial *= j
+        result[i] = factorial
+    return result
