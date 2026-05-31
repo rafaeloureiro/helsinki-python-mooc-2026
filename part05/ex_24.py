62169fa86e980cbc1159a6d37063c2fe39542df4
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-30 23:06:45 -0300

    A function to return the name of the eldest person in a tuple.

--- part05/ex_24.py
+++ part05/ex_24.py
@@ -1,3 +1,14 @@
 """
-MODELO
+A function named oldest_person(people: list), which takes a list of tuples as its argument. 
+In each tuple, the first element is the name of a person, and the second element is their year of birth. 
+The function should find the oldest person on the list and return their name.
 """
+
+def oldest_person(people: list):
+    oldest = people[0]
+    for person in people:
+        # comparing the year of birth of the oldest known person and the person in turn
+        if person[1] < oldest[1]:
+            oldest = person
+ 
+    return oldest[0]
