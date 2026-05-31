cf7b7b341739dc43146ad2a0fac1298c2bb1a23a
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-30 23:14:11 -0300

    A function that returns a list of the people on a given list that are elder than a given birthyear.

--- part05/ex_25.py
+++ part05/ex_25.py
@@ -1,3 +1,11 @@
 """
-MODELO
+A function named older_people(people: list, year: int), which selects all those people on the list who were born before the year given as an argument. 
+The function should return the names of these people in a new list.
 """
+
+def older_people(people: list, year: int):
+    older = []
+    for person in people:
+        if person[1] < year:
+            older.append(person[0])
+    return older
