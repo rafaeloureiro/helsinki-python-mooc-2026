739387e7243497a769784a2064c87cb2546c8de3
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-30 23:28:04 -0300

    A program which prints out a square of letters.

--- part05/ex_27.py
+++ part05/ex_27.py
@@ -1,3 +1,13 @@
 """
-MODELO
+A program which prints out a square of letters.
 """
+
+layers = int(input("Layers: "))
+size = 2 * layers - 1
+
+for row in range(size):
+    line = ""
+    for col in range(size):
+        distance = min(row, col, size - 1 - row, size - 1 - col)
+        line += chr(ord("A") + layers - 1 - distance)
+    print(line)
