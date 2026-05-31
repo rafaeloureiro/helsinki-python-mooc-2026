3ea59d82663e52c4e2b597063a58050c68a92cbc
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-05-30 22:54:56 -0300

    A functions that creates a tuple based on given conditions.

--- part05/ex_23.py
+++ part05/ex_23.py
@@ -1,3 +1,13 @@
 """
-MODELO
+A function named create_tuple(x: int, y: int, z: int), which takes three integers as its arguments, and creates and returns a tuple based on the following criteria:
+
+The first element in the tuple is the smallest of the arguments
+The second element in the tuple is the greatest of the arguments
+The third element in the tuple is the sum of the arguments
 """
+
+def create_tuple(x: int, y: int, z: int)->tuple[int, int, int]:
+   a = min(x,y,z)
+   b = max(x,y,z)
+   c = x + y + z
+   return (a, b, c)
