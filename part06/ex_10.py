3072ef4b6843c2a67be2f17d8b5cd5e1d4ee5b38
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-02 12:18:58 -0300

    A program that writes a new file.

--- part06/ex_10.py
+++ part06/ex_10.py
@@ -1,3 +1,9 @@
 """
-Modelo
+A program that writes a new file.
 """
+
+subject = input("Whom should I sign this to: ")
+file_name = input("Where shall I save it: ")
+
+with open(file_name, "w") as my_file:
+    my_file.write(f"Hi {subject}, we hope you enjoy learning Python with us! Best, Mooc.fi Team")
