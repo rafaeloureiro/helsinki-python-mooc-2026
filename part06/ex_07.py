5c72207f86f5b3f72e27020cdb6af67116805135
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-02 10:29:14 -0300

    A program that perform a spell check, and print out feedback to the user.

--- part06/ex_07.py
+++ part06/ex_07.py
@@ -1,3 +1,21 @@
 """
-Modelo
+A program which asks the user to type in some text. 
+Your program should then perform a spell check, and print out feedback to the user, so that all misspelled words have stars around them.
 """
+
+word_list = set()
+with open("wordlist.txt") as file:
+    for line in file:
+        word_list.add(line.strip())
+
+user_text = input("Write text: ")
+check = user_text.split()
+
+result = []
+for word in check:
+    if word.lower() not in word_list:
+        result.append(f"*{word}*")
+    else:
+        result.append(word)
+
+print(" ".join(result))
