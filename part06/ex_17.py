9115121d4bb036548423f5443eb25586797d9c7a
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-02 15:02:13 -0300

    a function named read_input, which asks the user for input until the user types in an integer which falls within the bounds given as arguments to the function.

--- part06/ex_17.py
+++ part06/ex_17.py
@@ -1,3 +1,16 @@
 """
-Model
+A function named read_input, which asks the user for input until the user types in an integer which falls within the bounds given as arguments to the function.
 """
+
+def read_input(prompt: str, lower: int, upper: int):
+    while True:
+        try:
+            number = int(input(prompt))
+
+            if lower <= number <= upper:
+                return number
+
+            print(f"You must type in an integer between {lower} and {upper}")
+
+        except ValueError:
+            print(f"You must type in an integer between {lower} and {upper}")
