eb7b9b494396775143428497b55a3ae818f471b3
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-05 13:34:42 -0300

    Password generation function to a given number of arguments.
    Add a function to generate passwords using lowercase letters.

--- part07/ex_05.py
+++ part07/ex_05.py
@@ -1,3 +1,10 @@
 """
-MODEL
+A function which creates passwords of a desired length, consisting of lowercase characters a to z.
 """
+
+from random import sample
+from string import ascii_lowercase
+
+def generate_password(n: int):
+    return "".join(sorted(sample(ascii_lowercase, n)))
+  
