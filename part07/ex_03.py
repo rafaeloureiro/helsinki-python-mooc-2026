63cc0a16714979629a6f037a3b943f9864650a43
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-05 12:07:19 -0300

    Fractionate function for equal fractions.
    Add fractionate function to create equal fractions

--- part07/ex_03.py
+++ part07/ex_03.py
@@ -1,3 +1,11 @@
 """
-MODEL
+A function named fractionate(amount: int), which takes the number of parts as its argument. 
+The function should divide the number 1 into as many equal sized fractions as is specified by the argument, and return these in a list.
 """
+
+from fractions import Fraction
+
+def fractionate(amount: int) -> list:
+    fraction = Fraction(1,amount)
+
+    return [fraction] * amount
