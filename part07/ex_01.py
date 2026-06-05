3ab388b5b14c9349c1adec0cc3d6db27627a0ec5
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-05 11:11:37 -0300

    A function to calculate the hypotenuse of a triangle.

--- part07/ex_01.py
+++ part07/ex_01.py
@@ -1,3 +1,9 @@
 """
-TESTE
+A function named hypotenuse(leg1: float, leg2: float), which takes the lengths of the two sides adjacent to the right angle of an orthogonal triangle. 
+The function should return the length of the hypotenuse, or the side opposite to the right angle.
 """
+
+from math import * 
+
+def hypotenuse(leg1: float, leg2: float):
+    return sqrt(leg1**2 + leg2**2)
