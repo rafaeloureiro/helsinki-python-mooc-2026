d83c472b67ee5d5e25d49150affe44748bd524f3
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-05 13:11:28 -0300

    lottery_numbers function for random number generation in a given range.
    Implement lottery_numbers function to generate random numbers.

--- part07/ex_04.py
+++ part07/ex_04.py
@@ -1,3 +1,11 @@
 """
-MODEL
+A function named lottery_numbers(amount: int, lower: int, upper: int), which generates as many random numbers as specified by the first argument. 
+All numbers should fall within the bounds lower to upper. The numbers should be stored in a list and returned. 
+The numbers should be in ascending order in the returned list.
 """
+
+from random import sample
+
+def lottery_numbers(amount: int, lower: int, upper: int):
+    numbers_range = list(range(lower, upper + 1))
+    return sorted(sample(numbers_range, amount))
