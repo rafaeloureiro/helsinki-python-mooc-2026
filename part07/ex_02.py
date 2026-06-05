7be63929cb40daf4b15df071d23fdd959f8c86e0
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-05 11:54:12 -0300

    Add function to separate characters in a string

--- part07/ex_02.py
+++ part07/ex_02.py
@@ -1,3 +1,20 @@
 """
 MODEL
 """
+
+from string import *
+def separate_characters(my_string: str):
+  
+    letters = ""
+    punctuation = ""
+    others = ""
+
+    for char in my_string:
+        if char in string.ascii_letters:
+            letters += char
+        elif char in string.punctuation:
+            punctuation += char
+        else:
+            others += char
+
+    return (letters, punctuation, others)
