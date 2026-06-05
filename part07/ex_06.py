127c8edc3f3cb4221ef781afff4aae2faa866512
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-05 16:47:30 -0300

    Improved version of password generator.
    Refactored password generation logic to improve security and flexibility.

--- part07/ex_06.py
+++ part07/ex_06.py
@@ -1,3 +1,32 @@
 """
-MODEL
+Improved version of password generator.
 """
+
+from random import choice, randint
+from string import ascii_lowercase, digits
+ 
+def generate_strong_password(length: int, numbers: bool, special_characters: bool):
+    special_chars = "!?=+-()#"
+    # One letter at beginning of the password
+    passwd = choice(ascii_lowercase)
+    choice_set = ascii_lowercase
+ 
+    if numbers:
+        passwd = add_character(passwd, digits)
+        choice_set += digits
+ 
+    if special_characters:
+        passwd = add_character(passwd, special_chars)
+        choice_set += special_chars
+ 
+    while (len(passwd) < length):
+        passwd = add_character(passwd, choice_set)
+ 
+    return passwd
+ 
+def add_character(passwd: str, characters):
+    character = choice(characters)
+    if randint(1,2) == 1:
+        return character + passwd
+    else:
+        return passwd + character
