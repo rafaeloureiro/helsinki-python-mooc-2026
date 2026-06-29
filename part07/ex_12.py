8ff42f777244ebc1bb6f82f77f032cd5401bfde8
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-29 11:00:14 -0300

    A function named print_persons(filename: str), which reads a JSON file in the format, and prints the contents.
    Added a function to read and print persons from a JSON file.

--- part07/ex_12.py
+++ part07/ex_12.py
@@ -1,3 +1,14 @@
 """
-TESTE
+A function named print_persons(filename: str), which reads a JSON file in the format, and prints the contents.
 """
+
+import json
+def print_persons(filename: str):
+    with open(filename) as f:
+        content = f.read()
+    persons = json.loads(content)
+    for person in persons:
+        name = person['name']
+        age = person['age']
+        hobbies = ", ".join(person['hobbies'])
+        print(f"{name} {age} years ({hobbies})")
