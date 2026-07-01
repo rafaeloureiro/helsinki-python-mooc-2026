7aed6797121897cefd6620f7cddfba4fe93b96d1
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-30 21:38:21 -0300

    Add DecreasingCounter class with basic methods

---
+++ part08/ex_10.py
@@ -0,0 +1,21 @@
+"""
+
+"""
+
+class DecreasingCounter:
+    def __init__(self, initial_value: int):
+        self.value = initial_value
+        self.initial_value = initial_value
+
+    def print_value(self):
+        print("value:", self.value)
+
+    def decrease(self):
+        if self.value >= 1:
+            self.value -= 1
+
+    def set_to_zero(self):
+        self.value = 0
+
+    def reset_original_value(self):
+        self.value = self.initial_value
