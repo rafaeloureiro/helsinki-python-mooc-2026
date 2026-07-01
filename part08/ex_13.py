7edd118f2d6f1577da1e164812008054ec2f9443
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-06-30 21:40:26 -0300

    Implement Person class with name handling methods
    Added Person class with methods to return first and last names.

---
+++ part08/ex_13.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+class Person:
+    def __init__(self, name: str):
+        self.name = name
+
+    def return_first_name (self):
+        names = self.name.split()
+        return names[0]
+    
+    def return_last_name (self):
+        names = self.name.split()
+        return names[-1]
