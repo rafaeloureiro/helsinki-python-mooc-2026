5a266e620eb63b2b98d3e7d6adb403992f6d1612
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-07 23:10:06 -0300

    class car with fastest_car function to find the fastest car
    Implemented a function to determine the fastest car from a list of Car objects.

---
+++ part09/ex_01.py
@@ -0,0 +1,21 @@
+"""
+A function named fastest_car(cars: list) which takes a list of Car objects as its argument.
+The function should return the make of the fastest car. You may assume there will always be a single car with the highest top speed.
+"""
+
+class Car:
+    def __init__(self, make: str, top_speed: int):
+        self.make = make
+        self.top_speed = top_speed
+
+    def __str__(self):
+        return f"Car (make: {self.make}, top speed: {self.top_speed})"
+
+def fastest_car(cars: list):
+
+    fastest = cars[0]
+
+    for car in cars:
+        if car.top_speed > fastest.top_speed:
+            fastest = car
+    return fastest.make
