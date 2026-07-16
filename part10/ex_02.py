b2855555576823769d0f5ce5cb91384dd2e70eaf
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-16 16:19:32 -0300

    Implement GameMuseum class extending GameWarehouse
    Added GameMuseum class to filter games made before 1990.

--- part10/ex_02.py
+++ part10/ex_02.py
@@ -1,3 +1,33 @@
 """
-
+A GameWarehouse object is used to store ComputerGame objects.
+A class named GameMuseum which inherits the GameWarehouse class.
+The GameMuseum class override the list_games() method and returns a list of only those games which were made before the year 1990.
+The class GameMuseum also have a constructor which calls the constructor from the parent class GameWarehouse. The constructor takes no arguments.
 """
+
+class ComputerGame:
+    def __init__(self, name: str, publisher: str, year: int):
+        self.name = name
+        self.publisher = publisher
+        self.year = year
+
+class GameWarehouse:
+    def __init__(self):
+        self.__games = []
+
+    def add_game(self, game: ComputerGame):
+        self.__games.append(game)
+
+    def list_games(self):
+        return self.__games
+    
+class GameMuseum(GameWarehouse):
+    def __init__(self):
+        super().__init__()  
+
+    def list_games(self):
+        old_games = []
+        for games in super().list_games():
+            if games.year < 1990:
+                old_games.append(games)
+        return old_games
