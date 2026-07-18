7d03878558b881ddba7dd2fa021c528f21507835
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-18 09:54:36 -0300

    Implement Square class inheriting from Rectangle.
    Added a Square class that inherits from Rectangle, maintaining the same attributes.

--- part10/ex_03.py
+++ part10/ex_03.py
@@ -1,3 +1,23 @@
 """
-
+A class named Square which inherits the Rectangle class. 
+The sides of a square are all of equal length, which makes the square a special case of the rectangle. 
+The new class should not contain any new attributes.
 """
+
+class Rectangle:
+    def __init__(self, width: int, height: int):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        return f"rectangle {self.width}x{self.height}"
+
+    def area(self):
+        return self.width * self.height
+
+class Square(Rectangle):
+    def __init__(self, side: int):
+        super().__init__(side, side)
+
+    def __str__(self):
+        return f"square {self.width}x{self.height}"
