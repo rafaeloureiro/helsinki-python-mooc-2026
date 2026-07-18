22abc0f9f9a5f2f469ad777b6152a74287b43ec6
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-18 11:07:33 -0300

    best_results function for ExamResult grades
    Implement a function to extract the best grades from ExamResult objects.

---
+++ part11/ex_03.py
@@ -0,0 +1,17 @@
+"""
+A function named best_results(results: list) which takes a list of ExamResult objects as its argument and return a new list containing only the best result from each ExamResult object. 
+"""
+
+class ExamResult:
+    def __init__(self, name: str, grade1: int, grade2: int, grade3: int):
+        self.name = name
+        self.grade1 = grade1
+        self.grade2 = grade2
+        self.grade3 = grade3
+
+    def __str__(self):
+        return (f'Name:{self.name}, grade1: {self.grade1}' +
+            f', grade2: {self.grade2}, grade3: {self.grade3}')
+    
+def best_results(results: list) -> list:
+    return [max(result.grade1, result.grade2, result.grade3) for result in results]
