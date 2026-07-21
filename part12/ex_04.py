dc92024797bd5a82a0b694c2eb40784283131723
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-21 00:06:56 -0300

    Add ClimbingRoute class and sorting functions

--- part12/ex_04.py
+++ part12/ex_04.py
@@ -1,3 +1,25 @@
 """
 
 """
+
+class ClimbingRoute:
+    def __init__(self, name: str, length: int, grade: str):
+        self.name = name
+        self.length = length
+        self.grade = grade
+
+    def __str__(self):
+        return f"{self.name}, length {self.length} metres, grade {self.grade}"
+
+
+def by_length (route: ClimbingRoute):
+    return route.length
+
+def sort_by_length(routes: list):
+    return sorted(routes, key=by_length,reverse=True)
+
+def by_difficulty(route: ClimbingRoute):
+    return (route.grade, route.length)
+
+def sort_by_difficulty(routes: list):
+    return sorted(routes, key=by_difficulty,reverse=True)
