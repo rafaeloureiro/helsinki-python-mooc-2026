5aefb45c4fbe661fd86b764fb839bcc291010c3a
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-26 00:17:50 -0300

    Implement LotteryNumbers class with hit evaluation methods.
    Added LotteryNumbers class with methods to evaluate lottery hits.

--- part11/ex_07.py
+++ part11/ex_07.py
@@ -1,3 +1,17 @@
 """
-
+A class named LotteryNumbers which takes the week number (an integer value) and a list of seven integers as its constructor arguments. 
+The list contains the correct lottery numbers for the given week.
+A method named number_of_hits(numbers: list) which takes a list of integers as its argument. 
+The method returns the number of correct entries in the parameter list.
 """
+
+class LotteryNumbers:
+    def __init__(self, week: int, numbers: list):
+        self.week = week
+        self.numbers = numbers
+
+    def number_of_hits(self, numbers: list) -> int:
+        return len([number for number in numbers if number in self.numbers])
+
+    def hits_in_place(self, numbers: list) -> list:
+        return [number if number in self.numbers else -1 for number in numbers]
