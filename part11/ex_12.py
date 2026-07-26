5a3ff516d9125a15002eec3295d6dbf604a6d9a4
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-26 00:00:48 -0300

    Add most_common_words function for word counting
    Implemented a function to count word occurrences in a file.

--- part11/ex_12.py
+++ part11/ex_12.py
@@ -1,3 +1,17 @@
 """
-
+A function named most_common_words(filename: str, lower_limit: int) that takes a filename and an integer value for a lower limit as its arguments. 
+The function should return a dictionary containing the occurrences of the words which appear at least the number of times specified in the lower_limit parameter.
 """
+
+def most_common_words(filename: str, lower_limit: int) -> dict:
+    with open(filename) as file:
+        text = file.read()
+
+    punctuation = ".,;:!?\"'()[]{}—-–…"
+    for char in punctuation:
+        text = text.replace(char, "")
+
+    words = text.split()
+
+    counts = {word: words.count(word) for word in set(words)}
+    return {word: count for word, count in counts.items() if count >= lower_limit}
