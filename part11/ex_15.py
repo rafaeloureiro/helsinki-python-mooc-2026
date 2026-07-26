cc5d0af530f6bb724e8a03f919b45658baba4a71
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-26 00:06:58 -0300

    Add balanced_brackets function for string validation
    Implement balanced_brackets function to check for balanced parentheses and brackets in a string.

--- part11/ex_15.py
+++ part11/ex_15.py
@@ -1,3 +1,18 @@
 """
-
+Function balanced_brackets which takes a string as its argument. 
+It checks if the brackets, or parentheses, within the string are balanced. That is, for each opening bracket ( there should be a closing bracket ), and
+all pairs of brackets should be matched in order, i.e. the bracket pairs must not be crossed.
 """
+
+def balanced_brackets(my_string: str) -> bool:
+    my_string = "".join(char for char in my_string if char in "()[]")
+
+    if len(my_string) == 0:
+        return True
+
+    pairs = {"(": ")", "[": "]"}
+
+    if my_string[0] not in pairs or my_string[-1] != pairs[my_string[0]]:
+        return False
+
+    return balanced_brackets(my_string[1:-1])
