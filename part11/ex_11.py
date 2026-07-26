92569fa2656d6607245a44e68e5a30217ec36807
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-25 23:58:57 -0300

    Add lengths function using dictionary comprehension
    Implement lengths function to return string lengths.

--- part11/ex_11.py
+++ part11/ex_11.py
@@ -1,3 +1,8 @@
 """
-
+A function named lengths(strings: list) which takes a list of strings as its argument. 
+The function returns a dictionary with the strings in the list as the keys and their lengths as the values.
+The function is implemented with a dictionary comprehension.
 """
+
+def lengths(strings: list) -> dict:
+    return {string: len(string) for string in strings}
