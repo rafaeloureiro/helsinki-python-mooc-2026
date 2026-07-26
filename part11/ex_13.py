2ab801ed33d00648b539dca16fab457946237df1
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-26 00:02:20 -0300

    Add recursive function to extend list of numbers
    Implemented a recursive function to add numbers to a list until its length is divisible by five.

--- part11/ex_13.py
+++ part11/ex_13.py
@@ -1,3 +1,9 @@
 """
-
+A recursive function named add_numbers_to_list(numbers: list). 
+The function takes a list of numbers as its argument, and adds new numbers to the list until the length of the list is divisible by five. 
+Each number added to the list should be one greater than the last number in the list.
 """
+def add_numbers_to_list(numbers: list):
+    if len(numbers) % 5 != 0:
+        numbers.append(numbers[-1] + 1)
+        add_numbers_to_list(numbers)
