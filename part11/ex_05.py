785d2a0c3d72f99f8d21ef960a3502c3083cb4ed
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-26 00:19:40 -0300

    Add function to remove numbers smaller than a limit
    Implement remove_smaller_than function to filter list.

--- part11/ex_05.py
+++ part11/ex_05.py
@@ -1,3 +1,7 @@
 """
-
+A function named remove_smaller_than(numbers: list, limit: int) which takes a list of integers and a limit value (also in integer format) as its arguments.
+The function use a list comprehension to produce a new list without the values which are smaller than the limit value.
 """
+
+def remove_smaller_than(numbers: list, limit: int) -> list:
+    return [number for number in numbers  if number >= limit]
