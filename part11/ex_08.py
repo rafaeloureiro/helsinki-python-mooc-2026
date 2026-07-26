64f5b2a5f280d78b6683d6c7299c8b496f11de8a
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-26 00:13:53 -0300

    Add filter_forbidden function.
    Implement filter_forbidden function to remove forbidden characters from a string.

--- part11/ex_08.py
+++ part11/ex_08.py
@@ -1,3 +1,8 @@
 """
-
+A function named filter_forbidden(string: str, forbidden: str) which takes two strings as its arguments. 
+The function should return a new version of the first string. It should not contain any characters from the second string.
 """
+
+def filter_forbidden(string: str, forbidden: str):
+    characters = [character for character in string if character not in forbidden]
+    return "".join(characters)
