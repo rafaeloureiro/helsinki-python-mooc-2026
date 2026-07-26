7fe7be544c8b1abd7d4c2041e4a282018d06e7dc
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-26 00:04:19 -0300

    Add recursive_sum function for summation
    Implement a recursive function to calculate the sum of numbers up to the given number.

--- part11/ex_14.py
+++ part11/ex_14.py
@@ -1,3 +1,8 @@
 """
-
+A recursive function named recursive_sum(number: int) which calculates the sum 1 + 2 + ... + number.
 """
+
+def recursive_sum(number: int):
+    if number <= 1:
+        return number
+    return number + recursive_sum(number -1)
