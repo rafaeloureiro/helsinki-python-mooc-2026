68bc876b5afc4c42804816e25b75da73b5170223
Rafael Loureiro <rafaeloureiro@hotmail.com>
2026-07-26 00:18:54 -0300

    Add function to return words starting with vowels.
    Implement function to filter words starting with vowels.

--- part11/ex_06.py
+++ part11/ex_06.py
@@ -1,3 +1,8 @@
 """
-
+A function named begin_with_vowel(words: list) which takes a list of strings as its argument.
+The function use a list comprehension technique to create and return a new list, containing only those words from the original list which begin with a vowel (a, e, i, o, u). 
+Both lowercase and uppercase letters should be accepted.
 """
+
+def begin_with_vowel(words: list) -> list:
+    return [word for word in words if word[0].lower() in "aeiou"]
